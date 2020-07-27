11d2aeb23d5adb70538598ec0eae76effc7eaecc
coolyashas <32161167+coolyashas@users.noreply.github.com>
2020-07-27 12:58:46 +0530

    Update toolbars.py

--- toolbars.py
+++ toolbars.py
@@ -3,7 +3,6 @@ from kivymd.uix.screen import Screen
 from kivy.lang import Builder
 from kivymd.uix.list import OneLineListItem
 
-
 screen_helper = """
 Screen:
     BoxLayout:
@@ -16,6 +15,7 @@ Screen:
                 left_action_items: [["coffee", lambda x: app.navigation_draw()]]
                 type:"bottom"
 """
+
 helper = """
 Screen:
     NavigationLayout:
@@ -27,11 +27,10 @@ Screen:
                         title:"Demo"
                         left_action_items: [['menu', lambda x: nav_drawer.toggle_nav_drawer()]]
                     Widget:
-
         MDNavigationDrawer:
             id:nav_drawer
             size_hint:(0.5,0.9)
-            
+
             Screen:
                 BoxLayout:
                     orientation:"vertical"
@@ -44,38 +43,44 @@ Screen:
                                 IconLeftWidget:
                                     icon:"android"
                             OneLineIconListItem:
+                                text:"Protests around you"
+                                IconLeftWidget:
+                                    icon:"android"
+                            OneLineIconListItem:
                                 text:"FAQ"
-                                icon:"android"
-                            OneLineIconListItem:
-                                text:"Item3"
                                 IconLeftWidget:
                                     icon:"android"
                             OneLineIconListItem:
-                                text:"Item4"
+                                text:"Your Protests"
                                 IconLeftWidget:
                                     icon:"android"
                             OneLineIconListItem:
-                                text:"Item5"
+                                text:"Protest merch"
                                 IconLeftWidget:
                                     icon:"android"
-
+                            OneLineIconListItem:
+                                text:"About us"
+                                IconLeftWidget:
+                                    icon:"android"
 """
 
-class DemoApp( MDApp ):
+
+class DemoApp(MDApp):
 
     def build(self):
-        screen = Builder.load_string( screen_helper )
+        screen = Builder.load_string(screen_helper)
         navigation = Builder.load_string(helper)
         screen.add_widget(navigation)
 
         return screen
 
     def navigation_draw(self):
-        print( "Navigation" )
-    def lv(self,obj):
-        for i in range( 20 ):
-            items = OneLineListItem( text="item" + str( i ) )
-            self.root.ids.listh.add_widget( items )
+        print("Navigation")
+
+    def lv(self, obj):
+        for i in range(20):
+            items = OneLineListItem(text="item" + str(i))
+            self.root.ids.listh.add_widget(items)
 
 
 DemoApp().run()
