255e8f59bf2b5b049683f179f2102527274ca259
Nihit13 <62506597+Nihit13@users.noreply.github.com>
2020-07-31 14:32:37 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,212 @@
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivy.lang import Builder
+from kivymd.uix.list import OneLineListItem
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+helper = """
+
+NavigationLayout:
+    ScreenManager:
+        id: screen_manager
+        Screen:
+            BoxLayout:
+                orientation: 'vertical'
+                MDToolbar:
+                    title: "Your app"
+                    elevation :5
+                    left_action_items: [['menu', lambda x: toot.toggle_nav_drawer()]]
+                Widget:
+                MDBottomAppBar:
+                    MDToolbar:
+                        title:"Help"
+                        left_action_items:[["help",lambda x:app.draw()]]
+                        type:"bottom"
+        Screen:
+            name: "screen2"
+            BoxLayout:
+                orientation: 'vertical'
+                MDToolbar:
+                    title: "Create"
+                    elevation:5
+                    left_action_items: [['menu', lambda x: toot.toggle_nav_drawer()]]
+                Widget:
+                MDBottomAppBar:
+                    MDToolbar:
+                        title:"Help"
+                        left_action_items:[["help",lambda x:app.draw()]]
+                        type:"bottom"
+            MDLabel:
+                text:"Name:"
+                pos_hint:{"center_x":0.6,"center_y":0.8}
+            MDTextField:
+                hint_text:"Enter your protest name"
+                pos_hint:{"center_x":0.6,"center_y":0.8}
+                size_hint:(0.7,0.1)
+            MDTextField:
+                multiline:True
+                hint_text: "Enter protest description"
+                mode:"rectangle"
+                pos_hint:{"center_x":0.6,"center_y":0.65}
+                size_hint:(0.65,0.2)
+
+            MDLabel:
+                text:"Description:"
+                pos_hint:{"center_x":0.53,"center_y":0.65}
+
+        Screen:
+            name: "screen3"
+            MDLabel:
+                text: "Screen 3"
+            BoxLayout:
+                orientation: 'vertical'
+                MDToolbar:
+                    title: "protest around"
+                    elevation:5
+
+                    left_action_items: [['menu', lambda x: toot.toggle_nav_drawer()]]
+                Widget:
+                MDBottomAppBar:
+                    MDToolbar:
+                        title:"Help"
+                        left_action_items:[["help",lambda x:app.draw()]]
+                        type:"bottom"
+
+        Screen:
+            name: "screen4"
+            MDLabel:
+                text: "Screen 4"
+            BoxLayout:
+                orientation: 'vertical'
+                MDToolbar:
+                    title: "FAQ"
+                    elevation:5
+                    left_action_items: [['menu', lambda x: toot.toggle_nav_drawer()]]
+                Widget:
+                MDBottomAppBar:
+                    MDToolbar:
+                        title:"Help"
+                        left_action_items:[["help",lambda x:app.draw()]]
+                        type:"bottom"
+
+        Screen:
+            name: "screen5"
+            MDLabel:
+                text: "Screen 5"
+            BoxLayout:
+                orientation: 'vertical'
+                MDToolbar:
+                    title: "Your protests"
+                    elevation:5
+                    left_action_items: [['menu', lambda x: toot.toggle_nav_drawer()]]
+                Widget:
+                MDBottomAppBar:
+                    MDToolbar:
+                        title:"Help"
+                        left_action_items:[["help",lambda x:app.draw()]]
+                        type:"bottom"
+
+        Screen:
+            name: "screen6"
+            MDLabel:
+                text: "Screen 6"
+            BoxLayout:
+                orientation: 'vertical'
+                MDToolbar:
+                    title: "protest merch"
+                    elevation:5
+                    left_action_items: [['menu', lambda x: toot.toggle_nav_drawer()]]
+                Widget:
+                MDBottomAppBar:
+                    MDToolbar:
+                        title:"Help"
+                        left_action_items:[["help",lambda x:app.draw()]]
+                        type:"bottom"
+
+        Screen:
+            name: "screen7"
+            MDLabel:
+                text: "Screen 7"
+            BoxLayout:
+                orientation: 'vertical'
+                MDToolbar:
+                    title: "about us"
+                    elevation:5
+                    left_action_items: [['menu', lambda x: toot.toggle_nav_drawer()]]
+                Widget:
+                MDBottomAppBar:
+                    MDToolbar:
+                        title:"Help"
+                        left_action_items:[["help",lambda x:app.draw()]]
+                        type:"bottom"
+    MDNavigationDrawer:
+
+        id:toot
+        BoxLayout:
+            orientation: 'vertical'
+            padding:"30dp"
+            spacing:"10dp"
+            ScrollView:
+                MDList:
+                    OneLineIconListItem:
+                        text: "Create"
+                        on_release:
+                            screen_manager.current = "screen2"
+                            toot.toggle_nav_drawer()
+                        IconLeftWidget:
+                            icon:"android"
+                    OneLineIconListItem:
+                        text: "protests around"
+                        on_release:
+                            screen_manager.current = "screen3"
+                            toot.toggle_nav_drawer()
+                        IconLeftWidget:
+                            icon:"android"
+
+                    OneLineIconListItem:
+                        text: "FAQ"
+                        on_release:
+                            screen_manager.current = "screen4"
+                            toot.toggle_nav_drawer()
+                        IconLeftWidget:
+                            icon:"android"
+
+                    OneLineIconListItem:
+                        text: "your protests"
+                        on_release:
+                            screen_manager.current = "screen5"
+                            toot.toggle_nav_drawer()
+                        IconLeftWidget:
+                            icon:"android"
+
+                    OneLineIconListItem:
+                        text: "protest merch"
+                        on_release:
+                            screen_manager.current = "screen6"
+                            toot.toggle_nav_drawer()
+                        IconLeftWidget:
+                            icon:"android"
+
+                    OneLineIconListItem:
+                        text: "About us"
+                        on_release:
+                            screen_manager.current = "screen7"
+                            toot.toggle_nav_drawer()
+                        IconLeftWidget:
+                            icon:"android"
+
+
+"""
+
+screen = Screen()
+
+
+class DemoApp( MDApp ):
+
+    def build(self):
+        app = Builder.load_string( helper )
+        screen.add_widget( app )
+        return screen
+
+
+DemoApp().run()
